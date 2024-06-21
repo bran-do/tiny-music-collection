c9aaf21106f511fa0d44d7a679b0f2a357ad2953
felipe <brandaofelipebrandao@gmail.com>
2024-06-20 21:30:52 -0300

    Add device shape/screen component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
+import Device from './canvas/Device/Device'
 
 function App() {
   return (
-    <h1>
-      Tiny Music Collection
-    </h1>
+    <Device />
   )
 }
 
---
+++ src/canvas/Device/Device.jsx
@@ -0,0 +1,14 @@
+import './Device.css'
+import Screen from './Screen/Screen'
+
+function Device() {
+  return (
+    <>
+      <div className="device_shape">
+        <Screen />
+      </div>
+    </>
+  )
+}
+
+export default Device
