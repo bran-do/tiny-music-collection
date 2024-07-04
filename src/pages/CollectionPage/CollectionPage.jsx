6ca852fa770ec12b379a83c70c946c1bdf2d6d6a
felipe <brandaofelipebrandao@gmail.com>
2024-07-04 19:40:47 -0300

    Add message for empty collection in CollectionPage

--- src/pages/CollectionPage/CollectionPage.jsx
+++ src/pages/CollectionPage/CollectionPage.jsx
@@ -1,12 +1,26 @@
+import { useContext } from 'react';
+
+import AppContext from '../../context/AppContext';
 import CollectionList from '../../components/CollectionList/CollectionList';
 import PageHeader from '../../components/PageHeader/PageHeader';
+
+import discIcon from '../../assets/icons/disc-small.svg'
 import './CollectionPage.css'
 
 function CollectionPage() {
+  const { collection } = useContext(AppContext)
+
   return (
     <>
       <PageHeader pageTitle={ "Collection" } />
-      <CollectionList />
+      {
+        collection.length != 0
+        ? <CollectionList />
+        : <div className='collection-empty'>
+            <img src={ discIcon } alt="" width="50px"/>
+            <h3>Add albums to your collection :)</h3>
+        </div>
+      }
     </>
   )
 }
