159e9aa1ce9b88289b2cff98d49c57d25767de03
felipe <brandaofelipebrandao@gmail.com>
2024-07-04 14:53:12 -0300

    Add icon and username to ProfilePage // AppContext now have "user" as default username

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 const AppContext = createContext({
-  username: '',
+  username: 'user', // Default username is 'user'
   setUsername: () => undefined,
 });
 
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,25 @@
+import { useContext } from 'react';
+
+import AppContext from '../../context/AppContext';
 import PageHeader from '../../components/PageHeader/PageHeader';
+
+import profileCircleIcon from '../../assets/icons/user-circle.svg'
 
 import './ProfilePage.css'
 
 function ProfilePage() {
+  const appContext = useContext(AppContext);
+
   return (
-    <PageHeader pageTitle={ "Profile" } />
+    <>
+      <PageHeader pageTitle={ "Profile" } />
+      <div className='profile-container'>
+        <div className='profile-icon-and-username'>
+          <img src={ profileCircleIcon } alt="Profile icon"/>
+          <h2>{ appContext.username }</h2>
+        </div>
+      </div>
+    </>
   )
 }
 
